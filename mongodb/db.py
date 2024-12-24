dac68c50437b88baba9090cf88eaf9bbad0e0736
GaneshAdimalupu <ganeshjohn253@gmail.com>
2024-12-24 22:26:41 +0530

    comment .env

--- mongodb/db.py
+++ mongodb/db.py
@@ -1,11 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 import urllib.parse  # Import the URL parsing library
 
 # Load environment variables
-load_dotenv()
+#load_dotenv()
 
 import streamlit as st
 
